77eac0aa5c05b9a26ae6e0150636bd9cc5886e58
ming0604 <b0981340647@gmail.com>
2024-09-30 02:29:56 +0800

    add use_carto_map para to gt_2D. recod not all topic

--- husky_navigation/src/gt_2D.cpp
+++ husky_navigation/src/gt_2D.cpp
@@ -2,11 +2,21 @@
 #include <nav_msgs/Odometry.h>
 
 ros::Publisher base_gt_2D_pub;
+bool use_carto_map;
 
 void gt_3D_callback(const nav_msgs::Odometry::ConstPtr& msg)
 {
     nav_msgs::Odometry gt_2D;
-    gt_2D = *msg;
+    if(use_carto_map)
+    {   
+        gt_2D = *msg;
+        gt_2D.pose.pose.position.x = msg->pose.pose.position.x-0.302;
+    }
+    else
+    {
+        gt_2D = *msg;
+    }
+    
     gt_2D.pose.pose.position.z = 0.0;
     base_gt_2D_pub.publish(gt_2D);
 }
@@ -15,9 +25,17 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv,"gt_2D");
     ros::NodeHandle nh;
+    ros::NodeHandle private_nh("~");
 
+    // Get the parameter whether to use the map constructed by Cartographer
+    private_nh.param("use_carto_map", use_carto_map, false);
+    if(use_carto_map)
+    {
+        ROS_WARN("Using carto map oringin calibration");
+    }
     ros::Subscriber base_gt_3D_sub = nh.subscribe("/ground_truth/state", 10, gt_3D_callback);
     base_gt_2D_pub = nh.advertise<nav_msgs::Odometry>("/base_gt",10);
+
 
     ros::spin();
     return 0;
