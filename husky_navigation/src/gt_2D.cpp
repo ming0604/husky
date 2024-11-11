f53e4f0ff863f348e8964657e7b6b75c619ba8a7
ming0604 <b0981340647@gmail.com>
2024-11-11 10:16:58 +0800

    add posestamped publisher

--- husky_navigation/src/gt_2D.cpp
+++ husky_navigation/src/gt_2D.cpp
@@ -1,11 +1,14 @@
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseStamped.h>
 
-ros::Publisher base_gt_2D_pub;
+ros::Publisher base_gt_2D_odom_pub;
+ros::Publisher base_gt_2D_pose_pub;
 bool use_carto_map;
 
 void gt_3D_callback(const nav_msgs::Odometry::ConstPtr& msg)
-{
+{   
+    //process the odometry message and publish it
     nav_msgs::Odometry gt_2D;
     if(use_carto_map)
     {   
@@ -18,7 +21,13 @@ void gt_3D_callback(const nav_msgs::Odometry::ConstPtr& msg)
     }
     
     gt_2D.pose.pose.position.z = 0.0;
-    base_gt_2D_pub.publish(gt_2D);
+    base_gt_2D_odom_pub.publish(gt_2D);
+
+    //publish PoseStamped msg
+    geometry_msgs::PoseStamped pose_msg;
+    pose_msg.header = msg->header;  // 複製時間戳和坐標系
+    pose_msg.pose = gt_2D.pose.pose;  // 複製已經處理過的位姿資訊
+    base_gt_2D_pose_pub.publish(pose_msg);
 }
 
 int main(int argc, char **argv)
@@ -34,8 +43,8 @@ int main(int argc, char **argv)
         ROS_WARN("Using carto map oringin calibration");
     }
     ros::Subscriber base_gt_3D_sub = nh.subscribe("/ground_truth/state", 10, gt_3D_callback);
-    base_gt_2D_pub = nh.advertise<nav_msgs::Odometry>("/base_gt",10);
-
+    base_gt_2D_odom_pub = nh.advertise<nav_msgs::Odometry>("/base_gt_odom",10);
+    base_gt_2D_pose_pub = nh.advertise<geometry_msgs::PoseStamped>("/base_gt",10);
 
     ros::spin();
     return 0;
